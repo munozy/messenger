087b466b863483dd73a78efaa253bca66eb90507
Yupanqui Munoz <yupanqui@gmail.com>
2018-05-11 17:22:47 +0200

    Implements Messager API: getAllMessages, getMessage and addMessage.

--- src/main/java/org/munozy/messenger/resources/MessageResource.java
+++ src/main/java/org/munozy/messenger/resources/MessageResource.java
@@ -2,22 +2,39 @@ package org.munozy.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 import org.munozy.messenger.model.Message;
 import org.munozy.messenger.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	public Message addMessage(Message message, @Context UriInfo uriInfo) {
+		return messageService.addMessage(message);
 	}
 }
--- src/main/java/org/munozy/messenger/service/MessageService.java
+++ src/main/java/org/munozy/messenger/service/MessageService.java
@@ -2,19 +2,47 @@ package org.munozy.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.munozy.messenger.database.DatabaseClass;
 import org.munozy.messenger.model.Message;
 
 public class MessageService {
+	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
 
+	public MessageService() {
+		messages.put(1L, new Message(1, "Iai galera!", "MunozY"));
+		messages.put(2L, new Message(2, "Fala papito!", "MunozY"));
+	}
+	
 	public List<Message> getAllMessages() {
-		Message m1 =  new Message(1L, "Iai galera!", "MunozY");
-		Message m2 =  new Message(1L, "Fala papito!", "MunozY");
-		
-		List<Message> messages = new ArrayList<>();
-		messages.add(m1);
-		messages.add(m2);
-		
-		return messages;
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		Message message = messages.get(id);
+		if (message == null) {
+			throw new RuntimeException("Message with id " + id + " not found");
+		}
+		return message;
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 }

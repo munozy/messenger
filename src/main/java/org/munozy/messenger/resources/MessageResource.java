7f1adf7d1d6935b69f31786f3ee3a9c0225657ae
Yupanqui Munoz <yupanqui@gmail.com>
2018-05-22 11:49:12 +0200

    Implements Messager API: updateMessage and deleteMessage.

--- src/main/java/org/munozy/messenger/resources/MessageResource.java
+++ src/main/java/org/munozy/messenger/resources/MessageResource.java
@@ -3,14 +3,14 @@ package org.munozy.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 import org.munozy.messenger.model.Message;
 import org.munozy.messenger.service.MessageService;
@@ -34,7 +34,20 @@ public class MessageResource {
 	}
 	
 	@POST
-	public Message addMessage(Message message, @Context UriInfo uriInfo) {
+	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
 	}
 }
